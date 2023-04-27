af1172ebab48f3d28e2209f8820d4b0102d19dac
Kiara Bickers <kiara@kiarabickers.com>
2023-04-27 10:47:07 -0700

    flask install

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ import asyncio
 from google_auth_session import initialize_app
 
 # Set up server
-if __name__ == '__main__':
+if __name__ == '__google_auth_session__':
     loop = asyncio.new_event_loop()
     app = loop.run_until_complete(initialize_app())
 
--- api/secrets-old/db_secrets.py
+++
@@ -1,2 +0,0 @@
-db_connection_string = "mongodb://mongo:GcUGQTWW5q09mobvzW0z@containers-us-west-61.railway.app:7462/"
-db_name = "CalendarCoach-Test"
